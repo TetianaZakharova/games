4a9869b5fca2f2218da25bb1a39b23424e0491c3
TetianaZakharova <kazakova.tanya@gmail.com>
2022-01-11 21:38:42 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { CardList } from './components/memo-game/CardList';
+import { CrossWord } from './components/memo-game/CrossWord';
+
+
+function App() {
+
+  return (
+    <div>
+    <CardList />
+    <CrossWord />
+    </div>
+  );
+}
+
+export default App;
